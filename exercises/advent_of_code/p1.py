a5b30f850ba96cbfe35309e55bf38cca3de4d779
Matthew Berry <matryan@live.unc.edu>
2020-12-01 17:11:52 -0500

    update

---
+++ exercises/advent_of_code/p1.py
@@ -0,0 +1,12 @@
+"""Advent of Code problem 1.
+Goal: Find the two entries that sum to 2020;
+what do you get if you multiply them together?
+"""
+
+__author__ = "matthewberry5"
+
+
+import pandas as pd 
+
+
+df = pd.read_csv('data/aoc_problem_1')
